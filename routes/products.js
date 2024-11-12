d3ec3b1de55466bac6458a0474e5c2900c58b7a0
ljwadd1 <w0488136@nscc.ca>
2024-11-12 12:28:00 -0400

    added scaffolding: public folder with nested images folder, routes folder with products.js and users.js files

---
+++ routes/products.js
@@ -0,0 +1,7 @@
+// TODO: Routing for...
+
+// get all products (../products/all)
+
+// get product by id (../products/:id)
+
+// purchase
---
+++ routes/users.js
@@ -0,0 +1,9 @@
+// TODO: Routing for...
+
+// signup (../users/signup)
+
+// login (../users/login)
+
+// logout (../users/logout)
+
+// get user session (../users/getSession)
