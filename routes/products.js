6e6683ba9fdfc5616584f5d7e3850d2b49186978
Ljwadd1 <w0488136@nscc.ca>
2024-11-21 14:27:51 -0400

    working on purchase route

--- routes/products.js
+++ routes/products.js
@@ -9,14 +9,13 @@ const prisma = new PrismaClient({
   });
   
 
-// TODO: Routing for...
-
 // get all products
 router.get('/all', async (req, res) => {
   const products = await prisma.product.findMany();
 
   res.json(products);
 })
+
 
 // get product by id
 router.get('/:product_id', async (req, res) => {
@@ -45,7 +44,51 @@ router.get('/:product_id', async (req, res) => {
   }
 
 })
+
+
+// TODO: Routing for...
 // purchase
+router.post('/purchase', async (req, res) => {
+  // Ensure user is logged in
+  if (req.session.email) {
+    //res.send('User is logged in');  // test
+    const { street, city, province, country, postal_code, credit_card, credit_expire, credit_cvv, cart, invoice_amt, invoice_tax, invoice_total } = req.body; // should cart be separate?
+  } else {
+    return res.status(401).send('You must be logged in to make a purchase.');
+  }
+
+  // Check if cart is empty ??
+  if (!cart) {
+    return res.status(400).send('Your cart is empty.')
+  }
+
+  // Input validation
+  if(!street || !city || !province || !country || !postal_code || !credit_card || !credit_expire || !credit_cvv || !invoice_amt || !invoice_tax || !invoice_total) {
+    return res.status(400).send('Bad request: All required fields must have a value.');
+  }
+
+  // If input is valid, create a new purchase
+  const purchase = await prisma.purchase.create({
+    data: {
+      customer_id: req.session.customer_id,   // ?
+      street: street,
+      city: city,
+      province: province,
+      country: country,
+      postal_code: postal_code,
+      credit_card: credit_card,
+      credit_expire: credit_expire,
+      credit_cvv: credit_cvv,
+      invoice_amt: invoice_amt,
+      invoice_tax: invoice_tax,
+      invoice_total: invoice_total
+    }
+  });
+
+  res.json(purchase);
+
+
+})
 
 
 export default router;
